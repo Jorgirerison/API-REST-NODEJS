a3f5044baa57ecf598b5bca725ee748f66c30adb
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-10-19 19:23:24 -0300

    feat:testando listagens de transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -20,5 +20,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies || [])
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
