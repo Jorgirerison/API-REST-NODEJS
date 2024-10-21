f9a2aceca53e2c3b2444b80671597169cfac433b
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-10-21 20:37:47 -0300

    feat:configurando banco de testes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { app } from './app'
 import { env } from './env'
 
-// Título: testando listagem de transações
-// Objetivo: objetivo da aula é além de criar o cookie p/dar seguimento a listagem é a de capturar e setar parametros das requisições e transações
+// Título: configurando banco de testes
+// Objetivo: segmentar ou criar um bd que o teste usa do bd que sobe para produção
 
 app
   .listen({
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { expect, it, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
