279ec2b53684b045fa8c91be4e424160544760f6
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-11-02 14:12:15 -0300

    chore: ajuste em client no database.ts

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
+  client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_CLIENT === 'sqlite'
       ? {
