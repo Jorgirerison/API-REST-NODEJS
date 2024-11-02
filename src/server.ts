35ec1a6b46fd60cbf765235e355ba8eafd03eb61
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-11-02 14:44:01 -0300

    feat: add host in server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP SERVER Running!')
